4e36217e4be0c747c375d5645a86fd52a0fb9f91
David-George Asaad <david.asaad@fides.it>
2023-06-09 15:12:55 +0200

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Inizio from "./lezioni/5-useRef/inizio/useref-example";
+import Start from "./lezioni/1-useState/inizio/1-errore-base";
 
 function App() {
   return (
     <div className="App">
       <section className="container text-center my-3">
-        <Inizio />
+        <Start />
       </section>
     </div>
   );
